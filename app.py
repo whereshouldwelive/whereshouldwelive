6e3bd5a152934c33af1d7051b93a93492a6ea02e
eleni <elenialevra98@gmail.com>
2020-02-08 16:24:40 +0000

    Fix cors 3

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template, request
 from flask_cors import CORS
 from geocoding import get_listings, get_result
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 global ppl_idx
 ppl_idx=['person_1','person_2','person_3']

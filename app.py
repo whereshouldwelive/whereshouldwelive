6288eb7328af505cfdfcb3016ea691b94c9e84cb
Jacob <32744010+yt1516@users.noreply.github.com>
2020-02-08 14:03:42 +0000

    store usr inputs

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def my_form():
+    ppl_idx=['1','2','3']
+    return render_template('index.html',ppl_idx=ppl_idx)
+
+@app.route('/usr_list', methods=['POST'])
+def my_form_post():
+    ppl_idx=['1','2','3']
+    ppl_dat=[]
+    for i in ppl_idx:
+        ppl_dat.append(request.form[i])
+    # text = request.form['text']
+    # processed_text = text.upper()
+    return render_template('usr_list.html',ppl_idx=ppl_idx,ppl_dat=ppl_dat)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)

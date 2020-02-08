eacf95698a1bd36b38f2bccdf6e9730b3b18345a
Jacob <32744010+yt1516@users.noreply.github.com>
2020-02-08 14:17:57 +0000

    changed app.py

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from geocoding import get_lat_long
 app = Flask(__name__)
 
+global ppl_idx
+ppl_idx=['person 1','person 2','person 3']
 
 @app.route('/')
 def my_form():
-    ppl_idx=['1','2','3']
     return render_template('index.html',ppl_idx=ppl_idx)
 
 @app.route('/usr_list', methods=['POST'])
 def my_form_post():
-    ppl_idx=['1','2','3']
+    # ppl_idx=['1','2','3']
     ppl_dat=[]
     for i in ppl_idx:
         ppl_dat.append(request.form[i])

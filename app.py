04068b138f6b5d4e2f08059f0e79cee4433ef4eb
Jacob <32744010+yt1516@users.noreply.github.com>
2020-02-08 14:28:05 +0000

    changed to get

--- app.py
+++ app.py
@@ -7,23 +7,21 @@ ppl_idx=['person_1','person_2','person_3']
 
 @app.route('/')
 def my_form():
-    print(ppl_idx)
     return render_template('index.html',ppl_idx=ppl_idx)
 
-@app.route('/usr_list', methods=['POST'])
+@app.route('/usr_list', methods=['get'])
 def my_form_post():
     # ppl_idx=['1','2','3']
     ppl_dat=[]
     for i in ppl_idx:
-        print(i)
-        ppl_dat.append(request.form[i])
+        ppl_dat.append(request.args.get(i))
     # text = request.form['text']
     # processed_text = text.upper()
     return render_template('usr_list.html',ppl_idx=ppl_idx,ppl_dat=ppl_dat)
 
-# @app.route('/find')
-# def find():
-#     return get_lat_long("SW7")
+@app.route('/find')
+def find():
+    return get_lat_long("SW7")
 
 if __name__ == '__main__':
     app.run(debug=True)
